e2157dbdf475414ec33e606d0037997df25cf1d8
Oladele <oladele2alo@gmail.com>
2022-02-17 11:07:01 +0100

    Data Models

---
+++ src/test/java/com/self/ecommerceApp/ecommerce/controllers/OrderControllerTest.java
@@ -0,0 +1,91 @@
+package com.self.ecommerceApp.ecommerce.controllers;
+
+
+import com.self.ecommerceApp.ecommerce.TestUtils;
+import com.self.ecommerceApp.ecommerce.model.persistence.Cart;
+import com.self.ecommerceApp.ecommerce.model.persistence.User;
+import com.self.ecommerceApp.ecommerce.model.persistence.UserOrder;
+import com.self.ecommerceApp.ecommerce.model.persistence.repositories.OrderRepository;
+import com.self.ecommerceApp.ecommerce.model.persistence.repositories.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class OrderControllerTest {
+
+    private OrderController orderController;
+    private OrderRepository orderRepository = mock(OrderRepository.class);
+    private UserRepository userRepository = mock(UserRepository.class);
+
+    @Before
+    public void setUp() {
+        orderController = new OrderController();
+        TestUtils.injectObjects(orderController, "orderRepository", orderRepository);
+        TestUtils.injectObjects(orderController, "userRepository", userRepository);
+    }
+
+    @Test
+    public void submit_order_happy_path(){
+        String existingUserName = "test";
+        User user = new User();
+
+        Cart cart = new Cart();
+        cart.setItems(new ArrayList<>());
+        cart.setTotal(BigDecimal.valueOf(2));
+        cart.setUser(user);
+
+        user.setCart(cart);
+
+        user.setUsername(existingUserName);
+        when(userRepository.findByUsername(existingUserName)).thenReturn(user);
+
+        ResponseEntity<UserOrder> response = orderController.submit(existingUserName);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(BigDecimal.valueOf(2), response.getBody().getTotal());
+    }
+
+    @Test
+    public void get_orders_for_user_happy_path(){
+        String existingUserName = "test";
+        User user = new User();
+        user.setUsername(existingUserName);
+
+        when(userRepository.findByUsername(existingUserName)).thenReturn(user);
+        when(orderRepository.findByUser(user)).thenReturn(new ArrayList<UserOrder>());
+
+        ResponseEntity<List<UserOrder>> response = orderController.getOrdersForUser(existingUserName);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(new ArrayList<UserOrder>(), response.getBody());
+    }
+
+    @Test
+    public void verify_submit_order_for_non_existing_user(){
+        String nonExistingUserName = "test5";
+
+        ResponseEntity<UserOrder> response = orderController.submit(nonExistingUserName);
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void verify_get_order_for_non_existing_user(){
+        String existingUserName = "test5";
+
+        ResponseEntity<List<UserOrder>> response = orderController.getOrdersForUser(existingUserName);
+
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
+    }
+}
---
+++ src/test/java/com/self/ecommerceApp/ecommerce/controllers/UserControllerTest.java
@@ -0,0 +1,100 @@
+package com.self.ecommerceApp.ecommerce.controllers;
+
+
+import com.self.ecommerceApp.ecommerce.TestUtils;
+import com.self.ecommerceApp.ecommerce.model.persistence.User;
+import com.self.ecommerceApp.ecommerce.model.persistence.repositories.CartRepository;
+import com.self.ecommerceApp.ecommerce.model.persistence.repositories.UserRepository;
+import com.self.ecommerceApp.ecommerce.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserControllerTest {
+    private UserController userController;
+    private BCryptPasswordEncoder bCryptPasswordEncoder = mock(BCryptPasswordEncoder.class);
+    private UserRepository userRepository = mock(UserRepository.class);
+    private CartRepository cartRepository = mock(CartRepository.class);
+
+    @Before
+    public void setUp() {
+        userController = new UserController();
+        TestUtils.injectObjects(userController, "userRepository", userRepository);
+        TestUtils.injectObjects(userController, "cartRepository", cartRepository);
+        TestUtils.injectObjects(userController, "bCryptPasswordEncoder", bCryptPasswordEncoder);
+    }
+
+    @Test
+    public void create_user_happy_path() {
+        when(bCryptPasswordEncoder.encode("testPassword")).thenReturn("thisIsHashed");
+        CreateUserRequest r = new CreateUserRequest();
+        r.setUsername("test");
+        r.setPassword("testPassword");
+        r.setConfirmPassword("testPassword");
+
+        final ResponseEntity<User> response = userController.createUser(r);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        User u = response.getBody();
+
+        assertNotNull(u);
+        assertEquals(0, u.getId());
+        assertEquals("test", u.getUsername());
+        assertEquals("thisIsHashed", u.getPassword());
+    }
+
+    @Test
+    public void verify_findByUserName() {
+        String existingUserName = "test";
+
+        User user = new User();
+        user.setUsername(existingUserName);
+
+        when(userRepository.findByUsername(existingUserName)).thenReturn(user);
+
+        final ResponseEntity<User> response = userController.findByUserName(existingUserName);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(user, response.getBody());
+
+    }
+
+    @Test
+    public void verify_findById() {
+        long existingUserId = 0;
+
+        User user = new User();
+        user.setId(0);
+        user.setUsername("test");
+
+        when(userRepository.findById(existingUserId)).thenReturn(Optional.of(user));
+
+        final ResponseEntity<User> response = userController.findById(existingUserId);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(user, response.getBody());
+    }
+
+    @Test
+    public void createUserTooShortPassword(){
+        CreateUserRequest r = new CreateUserRequest();
+        r.setUsername("test");
+        r.setPassword("test");
+        r.setConfirmPassword("test");
+
+        ResponseEntity<User> response = userController.createUser(r);
+
+        assertNotNull(response);
+        assertEquals(400, response.getStatusCodeValue());
+        assertNull(response.getBody());
+    }
+}
